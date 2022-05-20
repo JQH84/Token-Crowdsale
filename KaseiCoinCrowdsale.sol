2f2de1ded053063c0edd058680cb7bb5fd2b944d
Jihad Alhussain <jehad.alhussain@gmail.com>
2022-05-19 19:34:34 -0700

    addes optional code and fixed bug on line 63

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -4,20 +4,41 @@ import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
-
+// Adding timed , capped and refundable functions from openzeppelin
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
 
 // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
-contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
+contract KaseiCoinCrowdsale is
+    Crowdsale,
+    MintedCrowdsale,
+    CappedCrowdsale,
+    TimedCrowdsale,
+    RefundableCrowdsale
+{
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
+
+    // Adding the CappedCrowdsale , TimedCrowdsale and Refundable related varibales to the constructor and the respective constructors below
     constructor(
         uint256 rate,
         address payable wallet,
-        KaseiCoin token
-    ) public Crowdsale(rate, wallet, token) {
+        KaseiCoin token,
+        // extra varibles will go below
+        uint256 goal, // Crowdsale goal
+        uint256 open, // sale opening time
+        uint256 close // sale closing time
+    )
+        public
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(goal)
+        TimedCrowdsale(open, close)
+        RefundableCrowdsale(goal)
+    {
         // constructor can stay empty
     }
 }
@@ -32,19 +53,23 @@ contract KaseiCoinCrowdsaleDeployer {
     constructor(
         string memory name,
         string memory symbol,
-        address payable wallet
+        address payable wallet ,
+        uint goal // adding the goal 
     ) public {
         // Create a new instance of the KaseiCoin contract.
         KaseiCoin token = new KaseiCoin(name, symbol, 0);
 
         // Assign the token contract’s address to the `kasei_token_address` variable.
-        kasei_token_address = address(this);
+        kasei_token_address = address(token);
 
         // Create a new instance of the `KaseiCoinCrowdsale` contract
         KaseiCoinCrowdsale KAICrowdsale = new KaseiCoinCrowdsale(
             1,
             wallet,
-            token
+            token ,
+            goal,
+            now,
+            now + 24 weeks
         );
 
         // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
